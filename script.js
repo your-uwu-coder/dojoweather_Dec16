c72122dc59bbb9cacacc47d783133df813c1cff9
Lynn Ventura <m_lynnventura@yahoo.com>
2022-12-23 22:19:07 -0800

    New JS functions to DojoWeather

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ function accept() {
     closeCookie.remove();
 }
 
+function cel2Fah(temp) {
+    return Math.round(9/5 * temp + 32);
+}
+
+function fah2Cel(temp) {
+    return Math.round(5/9 * temp - 32);
+}
+
+function tempControl(element) {
+    for (var i=1; i<=8; i++) {
+        var temp = document.querySelector("#temp" + i);
+        var tempChange = temp.innerText;
+        if (element.value == "°C") {
+            temp.innerText = fah2Cel(tempChange);
+        } else {
+            temp.innerText = cel2Fah(tempChange);
+        }
+    }
+}
+
