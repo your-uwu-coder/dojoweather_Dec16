5f1b73fb07685f8e9065347a15d7485628e6cdce
Lynn Ventura <m_lynnventura@yahoo.com>
2022-12-23 23:06:13 -0800

    Fixed onchange function in JS

--- script.js
+++ script.js
@@ -7,22 +7,22 @@ function accept() {
     closeCookie.remove();
 }
 
-function cel2Fah(temp) {
+function c2f(temp) {
     return Math.round(9/5 * temp + 32);
 }
 
-function fah2Cel(temp) {
-    return Math.round(5/9 * temp - 32);
+function f2c(temp) {
+    return Math.round(5/9 * (temp - 32));
 }
 
 function tempControl(element) {
     for (var i=1; i<=8; i++) {
         var temp = document.querySelector("#temp" + i);
         var tempChange = temp.innerText;
-        if (element.value == "°C") {
-            temp.innerText = fah2Cel(tempChange);
+        if (element.value == "celsius") {
+            temp.innerText = f2c(tempChange);
         } else {
-            temp.innerText = cel2Fah(tempChange);
+            temp.innerText = c2f(tempChange);
         }
     }
 }
